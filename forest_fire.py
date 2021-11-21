d0a3d1e4b0ee142718e75e14d90fa6eac737245b
diptikaushal <74426908+diptikaushal@users.noreply.github.com>
2021-11-21 19:32:23 +0530

    Update forest_fire.py

--- forest_fire.py
+++ forest_fire.py
@@ -1,5 +1,3 @@
-#!C:\Users\Lenovo\AppData\Local\Programs\Python\Python37-32\python.exe
-
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
@@ -19,7 +17,6 @@ X = X.astype('int')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 log_reg = LogisticRegression()
 
-
 log_reg.fit(X_train, y_train)
 
 inputt=[int(x) for x in "45 32 60".split(' ')]
@@ -27,8 +24,5 @@ final=[np.array(inputt)]
 
 b = log_reg.predict_proba(final)
 
-
 pickle.dump(log_reg,open('model.pkl','wb'))
 model=pickle.load(open('model.pkl','rb'))
-
-
